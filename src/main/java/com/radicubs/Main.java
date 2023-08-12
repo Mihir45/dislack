08d165a99f44713d5806c91db02012cdb625fc25
Mihir Sood <soodmihir18@gmail.com>
2023-08-12 15:55:42 -0500

    discord bot api thingy not done

--- src/main/java/com/radicubs/Main.java
+++ src/main/java/com/radicubs/Main.java
@@ -1,7 +1,13 @@
 package com.radicubs;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hey");
+    static final String BOT_TOKEN = "MTE0MDAxOTE0MDA5NTEzNTc3NQ.G4F0nF.Zf-St1GkihbgaVTVeXt0NrYJvZvssRWcjC6g6E";
+    public static void main(String[] arguments) throws Exception
+    {
+        JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+        api.addEventListener(new DiscordListener());
     }
 }
