c6d50d2574a5af0d5a52c87e3add41ed14ced96a
Mihir Sood <soodmihir18@gmail.com>
2023-08-12 16:07:05 -0500

    bot token accidentally published to public github trololol

--- src/main/java/com/radicubs/Main.java
+++ src/main/java/com/radicubs/Main.java
@@ -2,12 +2,13 @@ package com.radicubs;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
-    static final String BOT_TOKEN = "MTE0MDAxOTE0MDA5NTEzNTc3NQ.G4F0nF.Zf-St1GkihbgaVTVeXt0NrYJvZvssRWcjC6g6E";
+    static final String BOT_TOKEN = "MTE0MDAxOTE0MDA5NTEzNTc3NQ.GBrK9k.mm689iw4ieC5WDZPTNYiexOhYYz9MbhygVstz8";
     public static void main(String[] arguments) throws Exception
     {
-        JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+        JDA api = JDABuilder.createDefault(BOT_TOKEN, GatewayIntent.GUILD_MESSAGES).build();
         api.addEventListener(new DiscordListener());
     }
 }
